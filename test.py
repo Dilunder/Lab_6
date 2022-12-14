8fcee812c282e24523fe865987eddbc3e3c4f002
Sechin Sergey Sergeevich <113019150+Dilunder@users.noreply.github.com>
2022-12-14 12:02:44 +0300

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from calc import resulting_expression
 
 class CalculatorTest(unittest.TestCase):
     def test_eval_expression(self):
-        self.assertEqual(resulting_expression('7-2'), 7 - 2)
+        self.assertEqual(resulting_expression('7-2'), 7 - 5)
         self.assertEqual(resulting_expression('5*5'), 5 * 5)
         self.assertEqual(resulting_expression('12/3'), 12 / 3)
         self.assertEqual(resulting_expression('1+1'), 1 + 1)
